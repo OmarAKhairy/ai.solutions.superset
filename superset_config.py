a9e54e48728b2cdda43d1902f7e256f3509c6c9f
Omar Khairy <omarkhairy@outlook.com>
2025-05-20 04:41:26 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,7 +1,7 @@
 import os
 
 # Secret key setup
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'temporary_key_replaced_at_runtime')
 
 # SQLite database by default (can be overridden)
 SQLALCHEMY_DATABASE_URI = os.environ.get(
@@ -14,13 +14,8 @@ FEATURE_FLAGS = {
     "ALERT_REPORTS": True
 }
 
-# Set low timeout for Cloud Run
-SQLALCHEMY_ENGINE_OPTIONS = {
-    "connect_args": {
-        "connect_timeout": 10
-    }
-}
-
 # Increase timeout for gunicorn workers
 SUPERSET_WEBSERVER_TIMEOUT = 300
 
+# Remove the problematic engine options
+# SQLALCHEMY_ENGINE_OPTIONS was causing the error
